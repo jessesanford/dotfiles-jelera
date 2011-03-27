f12f8b11991cf5d12987bcad1388874d01e2f78a
Jose Elera Campana <jelera@gmail.com>
2011-03-27 14:15:58 -0500

    added more feedback in mydotfiles.sh

--- mydotfiles.sh
+++ mydotfiles.sh
@@ -1,9 +1,49 @@
 #!/bin/bash
 
+#--------------------------------#
+#  backing up Existing dotfiles  #
+#--------------------------------#
+clear
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo "Backing your existing dotfiles (if any)"
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo ""
+echo ""
+echo "Copying them to ~/.backupdotfiles"
+
+mkdir ~/.backupdotfiles
+
+cp ~/.bashrc ~/.backupdotfiles/bashrc
+cp ~/.bash_profile ~/.backupdotfiles/bash_profile
+cp ~/.zshrc ~/.backupdotfiles/zshrc
+cp ~/.vimrc ~/.backupdotfiles/vimrc
+cp ~/.vim ~/.backupdotfiles/vim
+cp ~/.tmux.conf ~/.backupdotfiles/tmux.conf
+cp ~/.gitconfig ~/.backupdotfiles/gitconfig
+cp ~/.screenrc ~/.backupdotfiles/screenrc
+cp ~/.hgrc ~/.backupdotfiles/hgrc
+
+rm -f ~/.bashrc
+rm -f ~/.bash_profile
+rm -f ~/.hgrc
+rm -rf ~/.vim
+rm -f ~/.gitconfig
+rm -f ~/.zshrc
+rm -f ~/.vimrc
+rm -f ~/.tmux.conf
+rm -f ~/.screenrc
+
+echo "Backup done"
+sleep 1.5
+clear
 #-----------------------#
 #  Create the symlinks  #
 #-----------------------#
 
+echo "^^^^^^^^^^^^^^^^^^^^^"
+echo "Creating the Symlinks"
+echo "^^^^^^^^^^^^^^^^^^^^^"
+echo ""
 # Vim
 ln -s $HOME/dotfiles/vim  $HOME/.vim
 ln -s $HOME/dotfiles/vim/vimrc  $HOME/.vimrc
@@ -30,17 +70,32 @@ ln -s $HOME/dotfiles/git/gitconfig  $HOME/.gitconfig
 # Mercurial
 ln -s $HOME/dotfiles/hg/hgrc $HOME/.hgrc
 
+echo "Creating symlinks done"
+sleep 1.5
+clear
 
 #------------------------------------#
 #  Update Vim plugin git submodules  #
 #------------------------------------#
 
-cd dotfiles
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo "Updating Vim Plugins via git submodules"
+echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+echo ""
+
+cd ~/dotfiles
 git submodule init
 git submodule update
+
+echo "Vim Plugins are ready"
+sleep 1.5
+clear
 
 echo ""
 echo "If you get an error with git, install it and run again"
 echo ""
-echo "git submodule init"
-echo "git submodule update"
+echo "$ git submodule init"
+echo "$ git submodule update"
+echo ""
+echo ""
+echo "Enjoy!!"
