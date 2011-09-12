1048c80e68c078fabfbd62e5b99992c0a3432021
Jose Elera Campana <jelera@gmail.com>
2011-09-12 03:12:11 -0500

    cleanup of installmac.sh

--- installmac.sh
+++ installmac.sh
@@ -1,7 +1,25 @@
 #! /bin/bash
+clear
 echo "Don't forget to install XCode and Java Developer Update"
 echo "======================================================="
+echo ""
+echo "(If not... Ctrl-C to cancel)"
+sleep 4
+clear
+
+echo "Installing Homebrew Package Manager for OSX"
+echo "==========================================="
+
+# It fetches homebrew from github
+/usr/bin/ruby -e "$(curl -fsSL https://raw.github.com/gist/323731)"
+echo ""
+echo "done ...Installing Homebrew Package Manager for OSX"
 sleep 3
+
+clear
+
+echo "Installing GNU Coreutils and adding the symlinks to ~/bin folder"
+echo "================================================================"
 
 # Install latest GNU Coreutils and get the symlinks in $HOME/bin
 brew install coreutils
@@ -12,33 +30,28 @@ cd $HOME/bin
 for command in base64 basename cat chcon chgrp chmod chown chroot cksum comm cp csplit cut date dd df dir dircolors dirname du echo env expand expr factor false fmt fold groups head hostid id install join kill link ln logname ls md5sum mkdir mkfifo mknod mktemp mv nice nl nohup od paste pathchk pinky pr printenv printf ptx pwd readlink rm rmdir runcon seq sha1sum sha224sum sha256sum sha384sum sha512sum shred shuf sleep sort split stat stty sum sync tac tail tee test touch tr true tsort tty uname unexpand uniq unlink uptime users vdir wc who whoami yes "["; do
   ln -s "/usr/local/bin/g$command" "$command"
 done
+echo ""
+echo "done ...Installing GNU Coreutils and adding the symlinks to ~/bin folder"
+sleep 2
+clear 
 
-# Install the rest of important brews
+echo "Install the rest of important brews"
+echo "==================================="
+
 brew install ack \
 	agrep \
 	apachetop \
-	asciidoc \
 	aspell \
 	bash-completion \
 	bash \
-	bazaar \
-	bibutils \
 	binutils \
-	byobu \
-	cabextract \
 	closure-compiler \
 	csstidy \
 	ctags \
 	fortune \
-	ffmpeg \
 	findutils \
 	gawk \
 	git \
-	git-extras \
-	git-flow \
-	git-hg \
-	git-ssh \
-	git-utils \
 	gnu-sed \
 	html-xml-utils \
 	htmlcompressor \
@@ -52,11 +65,9 @@ brew install ack \
 	moreutils \
 	mplayer \
 	mutt \
-	oniguruma \
 	par \
 	rename \
 	renameutils \
-	spidermonkey \
 	subversion \
 	task \
 	tmux \
@@ -67,13 +78,36 @@ brew install ack \
 	yuicompressor \
 	zsh \
 	
-# Install NPM
-curl http://npmjs.org/install.sh | sh
+echo ""
+echo "done ... Installing extra brews"
+sleep 2
+clear
 
-# Install Rvm
+echo "Install Node Version Manager (NVM)"
+echo "=================================="
+git clone git://github.com/creationix/nvm.git ~/.nvm
+
+echo ""
+echo "done ... Installing nvm"
+sleep 2
+clear
+
+echo "Install Ruby Version Manager (RVM)"
+echo "=================================="
 bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
 
-# Install MacVim and updated Vim for the Terminal
+echo ""
+echo "done ... Installing rvm"
+sleep 2
+clear
+
+echo "Install MacVim and updated Vim for the Terminal"
 brew install macvim
-echo "`ls /usr/local/Cellar/macvim/*/MacVim.app/Contents/MacOS/Vim | head -n 1` \$*" > /usr/local/bin/vim
-chmod 755 /usr/local/bin/vim
+
+
+echo ""
+echo "done ... Installing MacVim"
+sleep 2
+clear
+
+echo "Enjoy and don't forget to setup nvm and rvm"
